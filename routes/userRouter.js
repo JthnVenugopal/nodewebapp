2aa3ebe4088eecf28e746a90867b8a03cfc996af
Jithin Venugopal <jithinvenugopaltz@gmail.com>
2024-11-09 12:05:05 +0530

    project initialize

---
+++ routes/userRouter.js
@@ -0,0 +1,9 @@
+const express =  require("express");
+const router = express.Router();
+const userController = require("../controllers/user/userController")
+
+//path to join home page 
+router.get("/", userController.loadHomepage);
+
+
+module.exports = router;
