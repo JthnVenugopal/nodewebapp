13eb48b8dcc3291596a848c840ea5c8c891dd440
Jithin Venugopal <jithinvenugopaltz@gmail.com>
2024-11-11 20:18:22 +0530

    Process initialized

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
 const express  =  require ('express');
 const app =  express();
-const path = require('path')
+const path = require('path');
 const env  = require('dotenv').config();
-const DB =  require("./config/db")
+const session = require("express-session");
+const DB =  require("./config/db");
 const port = process.env.PORT || 3001;
-const userRouter = require("./routes/userRouter")
+const userRouter = require("./routes/userRouter");
+
+
+
 DB()
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 
+app.use(session({
+  secret : process.env.SESSION_SECRET,
+  resave:false,
+  saveUninitialized:true,
+  cookie:{
+    secure:false,
+    httpOnly:true,
+    maxAge:72*60*60*1000
+  }
+}))
+
 app.set("view engine", "ejs");
 app.set("views", [path.join(__dirname, "views/user"),path.join(__dirname, "views/admin")]);
+
+app.use(express.static('public'));
 
 app.use(express.static(path.join(__dirname,"public")));
 
--- controllers/user/userController.js
+++ controllers/user/userController.js
@@ -1,4 +1,6 @@
 const User = require("../../models/userSchema");
+const nodemailer = require("nodemailer");
+const env = require("dotenv").config();
 
 const pageNotFound = async (req,res) => {
   try{
@@ -37,25 +39,95 @@ const loadShopping = async (req,res) => {
   }
 }
 
-const signup = async (req,res)=>{
-  const {name,email,phone,password,confirmPassword} = req.body;
+// const signup = async (req,res)=>{
+//   const {name,email,phone,password,confirmPassword} = req.body;
 
   
-  try{
+//   try{
        
-      const newUser = new User({name,email,phone, password, confirmPassword});
+//       const newUser = new User({name,email,phone, password, confirmPassword});
 
-      await newUser.save();
+//       await newUser.save();
 
-      return res.redirect("/signup")
+//       return res.redirect("/signup")
+
+//   }catch(error){
+
+//      console.error("Error for save user",error) ;
+
+//      res.status(500).send("Internal server error")
+
+//   }
+// }
+
+function generateOtp(){
+  return Math.floor(100000 + Math.random()*900000).toString();
+}
+
+async function sendVerificationEmail(email,otp){
+  try{
+     
+       const transporter = nodemailer.createTransport({
+          service:"gmail",
+          port:587,
+          secure: false,
+          requireTLS:true,
+          auth:{
+            user:process.env.NODEMAILER_EMAIL,
+            pass:process.env.NODEMAILER_PASSWORD
+          }
+
+       });
+
+       const info = await transporter.sendMail({
+        from:process.env.NODEMAILER_EMAIL,
+        to:email,
+        subject:"Verify Your Account",
+        text:`Your verification code is ${otp}`,
+        html:`<b>Your OTP : ${otp}</b>`,
+
+       });
+
+       return info.accepted.length > 0
 
   }catch(error){
+        console.error("Error sending email",error);
+        return false;
+  }
+}
 
-     console.error("Error for save user",error) ;
+const signup = async (req,res) => {
+  try{
+     const {name, email,phone, password, confirmPassword} = req.body;
 
-     res.status(500).send("Internal server error")
+     if(password !== confirmPassword ){
+       return res.render("signup",{message:"Password do not match"});
+     }
+
+     const findUser = await User.findOne({email});
+     if(findUser){
+      return res.render("signup",{message:"User with this email already exist "});
+     }
+
+     const otp = generateOtp();
+
+     const emailSent = await sendVerificationEmail(email,otp);
+     if(!emailSent){
+      return res.json("email.error")
+     }
+     req.session.userOTP = otp;
+     req.session.userData = {email,password};
+
+    //  res.render("verify-otp");
+     console.log("OTP Sent",otp);
+
+  } catch (error) {
+
+    console.error("Error signing up", error);
+    res.redirect("pageNotFound");
 
   }
+
 }
 
 module.exports = {
