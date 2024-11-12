d12eb2f2a3a4d0a9e1a550c91791210941ce5e6e
Jithin Venugopal <jithinvenugopaltz@gmail.com>
2024-11-13 01:29:28 +0530

    process initialized

--- app.js
+++ app.js
@@ -32,9 +32,8 @@ app.use(passport.session());
 
 app.set("view engine", "ejs");
 app.set("views", [path.join(__dirname, "views/user"),path.join(__dirname, "views/admin")]);
-app.use(express.static('public'));
 
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", userRouter)
 
